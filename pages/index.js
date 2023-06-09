ad7ad6260888edf0405e58ccfb702e50b0a71ae9
Krzysztof Skwarcan <krissrox190@gmail.com>
2023-06-09 14:17:47 +0200

    added things

---
+++ pages/index.js
@@ -0,0 +1,41 @@
+import Head from 'next/head';
+import Layout, { siteTitle } from '../components/layout';
+import utilStyles from '../styles/utils.module.css';
+import Link from 'next/link';
+import Date from '../components/date';
+
+import { getSortedPostsData } from '../lib/posts';
+
+export async function getStaticProps() {
+  const allPostsData = getSortedPostsData();
+  return {
+    props: {
+      allPostsData,
+    },
+  };
+}
+
+export default function Home({ allPostsData }) {
+  return (
+    <Layout home>
+      <Head>
+        <title>Blog</title>
+      </Head>
+      <p className={utilStyles.headingMd}>Did you know there is a species of jellyfish that never dies? Turritopsis dohrnii aka The Immortal Jellyfish is known to be able to revert back into its adolescent state after going through adulthood—making it basically eternal!</p>
+      <section className={`${utilStyles.headingMd} ${utilStyles.padding1px}`}>
+        <h2 className={utilStyles.headingLg}>Blog</h2>
+        <ul className={utilStyles.list}>
+          {allPostsData.map(({ id, date, title }) => (
+            <li className={utilStyles.listItem} key={id}>
+              <Link href={`/posts/${id}`}>{title}</Link>
+              <br />
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
+            </li>
+          ))}
+        </ul>
+      </section>
+    </Layout>
+  );
+}
